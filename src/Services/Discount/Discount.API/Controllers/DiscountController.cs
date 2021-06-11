4febdf8edbc871b618c557768b21addadd4d58ad
Sergio Camacho <sergiocamacho0113@gmail.com>
2021-06-11 14:05:03 -0500

    Create Discount API initial project and added postgres and pgadmin to docker compose services

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Discount.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DiscountController : ControllerBase
+    {
+        private readonly ILogger<DiscountController> _logger;
+
+        public DiscountController(ILogger<DiscountController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
