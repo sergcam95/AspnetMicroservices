2b5747ef3a4ec338044cba3a8fc36e6c0dabbec8
Sergio Camacho <sergiocamacho0113@gmail.com>
2021-06-24 13:37:08 -0500

    Migrations/Seed added when Discount API loads

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Discount.API.Extensions;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Discount.API
 {
@@ -13,7 +8,12 @@ namespace Discount.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            host.MigrateDatabase<Program>();
+            
+            
+            // Runs the app
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
